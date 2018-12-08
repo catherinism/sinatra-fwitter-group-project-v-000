d2f5f27dfdf8f6c74e52bae1e91f884c6e08d70f
Catherine Salazar <catcreates13@gmail.com>
2018-12-08 02:21:31 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,46 @@
+require 'pry'
+
 class UsersController < ApplicationController
+
+  get '/signup' do
+    if logged_in?
+      redirect "/tweets"
+    else
+    erb :'users/create_user'
+  end
+end
+
+post '/signup' do
+  #binding.pry
+  @user = User.create(params)
+  session[:id] = @user.id
+  if params[:username].empty? || params[:email].empty? || params[:password].empty?
+    redirect "/signup"
+  else
+    redirect "/tweets"
+  end
+end
+
+  get '/login' do
+    if logged_in?
+      redirect "/tweets"
+    else
+    erb :'users/login'
+  end
+end
+
+post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user
+      session[:user_id] = @user[:id]
+      redirect "/tweets"
+    else
+      redirect "/signup"
+    end
+  end
+
+
+
 
 
 end
