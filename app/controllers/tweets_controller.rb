bdf5a6799da99871eeba3c17e7af0c7215422b9e
Catherine Salazar <catcreates13@gmail.com>
2018-12-17 03:49:40 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -26,6 +26,7 @@ class TweetsController < ApplicationController
       #binding.pry
       if params[:content].empty?
         redirect :'/tweets/new'
+<<<<<<< HEAD
       else
         @user = current_user
         @tweet = Tweet.create(params)
@@ -71,6 +72,62 @@ end
     redirect to '/tweets'
   else
     redirect to '/login'
+=======
+      else
+        @user = current_user
+        @tweet = Tweet.create(params)
+        @user.tweets << @tweet
+      redirect :"/tweets/#{@tweet.id}"
+    end
+  end
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    if logged_in?
+      erb :'/tweets/show_tweet'
+    else
+      redirect :"/login"
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    #binding.pry
+    #raise params.inspect
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      @user = current_user
+      if @tweet.user_id == @user.id
+        erb :'/tweets/edit_tweet'
+      else
+        redirect :'/tweets'
+      end
+    else
+      redirect :'/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if params[:content].empty?
+      redirect :"/tweets/#{@tweet.id}/edit"
+    else
+      @tweet.update(content: params[:content])
+      redirect :"/tweets/#{@tweet.id}"
+    end
+  end
+
+  delete '/tweets/:id/delete' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if logged_in?
+      @user = current_user
+      if @tweet.user_id == @user.id
+        @tweet.delete
+        redirect to '/tweets'
+      end
+    else
+      redirect to '/login'
+    end
+>>>>>>> 303057f6e5bbb83536f63a6522cd3da060c46333
   end
 end
 
